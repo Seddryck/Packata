5da8044238bec672fd3dce4a310db717473c5d18
Cédric L. Charlier <seddryck@gmail.com>
2025-01-06 23:37:27 +0100

    fix: handle value "default" for format of types date, time, datetime (#33)

---
+++ src/Packata.Core/ResourceReading/DefaultFormatMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packata.Core.ResourceReading;
+public class DefaultFormatMapper
+{
+    public DefaultFormatMapper()
+    {
+        AddMapping("datetime", "%Y-%m-%dT%H:%M:%S");
+        AddMapping("date", "%Y-%m-%d");
+        AddMapping("time", "%H:%M:%S");
+    }
+
+    private Dictionary<string, string> _mappings = [];
+    public void AddMapping(string type, string defaultFormat)
+    {
+        if (!_mappings.TryAdd(type, defaultFormat))
+            _mappings[type] = defaultFormat;
+    }
+    public bool TryGetMapping(string type, [NotNullWhen(true)] out string? format)
+        => _mappings.TryGetValue(type, out format);
+}
