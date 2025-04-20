0a2f727660f8fe77c94512a8c65ec0f39d952a15
Cédric L. Charlier <seddryck@gmail.com>
2025-04-20 11:19:28 +0200

    feat: implementation of GetName, GetOrdinal and indexer by name for Excel

--- src/Packata.ResourceReaders.Testing/Tabular/SpreadsheetReaderTests.cs
+++ src/Packata.ResourceReaders.Testing/Tabular/SpreadsheetReaderTests.cs
@@ -45,7 +45,6 @@ public class SpreadsheetReaderTests
         var dataReader = reader.ToDataReader(resource);
 
         Assert.That(dataReader, Is.Not.Null);
-        Assert.That(dataReader, Is.InstanceOf<IExcelDataReader>());
         Assert.That(dataReader.Read(), Is.True);
         using (Assert.EnterMultipleScope())
         {
@@ -90,18 +89,49 @@ public class SpreadsheetReaderTests
     public void ToDataReader_ExistingLocalResourceWithName_ReturnsIDataReader(IPath path)
     {
         var resource = new Resource() { Paths = [path], Type = "table", Name = "my-resource" };
-        var wrapper = new ExcelReaderWrapper(new TableSpreadsheetDialect() { SheetNumber=null, SheetName = "Country" });
+        var wrapper = new ExcelReaderWrapper(new TableSpreadsheetDialect() { SheetNumber = null, SheetName = "Country", Header = true });
         var reader = new SpreadsheetReader(wrapper);
         var dataReader = reader.ToDataReader(resource);
 
         Assert.That(dataReader, Is.Not.Null);
-        Assert.That(dataReader, Is.InstanceOf<IExcelDataReader>());
         Assert.That(dataReader.Read(), Is.True);
         using (Assert.EnterMultipleScope())
         {
             Assert.That(dataReader[0], Is.EqualTo("be"));
             Assert.That(dataReader[1], Is.EqualTo("Belgium"));
             Assert.That(dataReader[2], Is.EqualTo("Brussels"));
+        }
+        for (int i = 1; i < 5; i++)
+            Assert.That(dataReader.Read(), Is.True);
+        Assert.That(dataReader.Read(), Is.False);
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetPaths))]
+    public void ToDataReader_ExistingLocalResourceWithName_ReturnsFieldNames(IPath path)
+    {
+        var resource = new Resource() { Paths = [path], Type = "table", Name = "my-resource" };
+        var wrapper = new ExcelReaderWrapper(new TableSpreadsheetDialect() { SheetNumber = null, SheetName = "Country", Header = true });
+        var reader = new SpreadsheetReader(wrapper);
+        var dataReader = reader.ToDataReader(resource);
+
+        Assert.That(dataReader, Is.Not.Null);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(dataReader.GetName(0), Is.EqualTo("Code"));
+            Assert.That(dataReader.GetName(1), Is.EqualTo("Name"));
+            Assert.That(dataReader.GetName(2), Is.EqualTo("Capital"));
+
+            Assert.That(dataReader.GetOrdinal("Code"), Is.EqualTo(0));
+            Assert.That(dataReader.GetOrdinal("Name"), Is.EqualTo(1));
+            Assert.That(dataReader.GetOrdinal("Capital"), Is.EqualTo(2));
+        }
+        Assert.That(dataReader.Read(), Is.True);
+        using (Assert.EnterMultipleScope())
+        {
+            Assert.That(dataReader["Code"], Is.EqualTo("be"));
+            Assert.That(dataReader["Name"], Is.EqualTo("Belgium"));
+            Assert.That(dataReader["Capital"], Is.EqualTo("Brussels"));
         }
         for (int i = 1; i < 5; i++)
             Assert.That(dataReader.Read(), Is.True);
---
+++ src/Packata.ResourceReaders/Tabular/ExcelDataReader.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDataReader;
+
+namespace Packata.ResourceReaders.Tabular;
+internal class ExcelDataReader : IDataReader
+{
+    private readonly IExcelDataReader _reader;
+    private readonly string[] _fieldNames;
+    private bool _isClosed = false;
+
+    public ExcelDataReader(IExcelDataReader reader, string[] fieldNames)
+    {
+        _reader = reader;
+        _fieldNames = fieldNames;
+    }
+
+    public int Depth => 0;
+
+    public bool IsClosed => _isClosed;
+
+    public int RecordsAffected => -1;
+
+    public int FieldCount
+        => _reader.FieldCount;
+
+    public object this[string name]
+        => _reader[GetOrdinal(name)];
+
+    public object this[int i]
+        => _reader[i];
+
+    public void Close()
+    {
+        _reader.Close();
+        _isClosed = true;
+    }
+
+    public DataTable? GetSchemaTable()
+        => throw new NotImplementedException();
+
+    public bool NextResult()
+        => _reader.NextResult();
+
+    public bool Read()
+        => _reader.Read();
+
+    public void Dispose()
+        => _reader.Dispose();
+
+    public bool GetBoolean(int i)
+        => _reader.GetBoolean(i);
+    public byte GetByte(int i) => throw new NotImplementedException();
+    public long GetBytes(int i, long fieldOffset, byte[]? buffer, int bufferoffset, int length) => throw new NotImplementedException();
+    public char GetChar(int i) => throw new NotImplementedException();
+    public long GetChars(int i, long fieldoffset, char[]? buffer, int bufferoffset, int length) => throw new NotImplementedException();
+    public IDataReader GetData(int i)
+        => _reader.GetData(i);
+    public string GetDataTypeName(int i)
+        => _reader.GetDataTypeName(i);
+    public DateTime GetDateTime(int i)
+        => _reader.GetDateTime(i);
+    public decimal GetDecimal(int i)
+        => _reader.GetDecimal(i);
+    public double GetDouble(int i)
+        => _reader.GetDouble(i);
+    [return: DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicFields | DynamicallyAccessedMemberTypes.PublicProperties)]
+    public Type GetFieldType(int i)
+        => _reader.GetFieldType(i);
+    public float GetFloat(int i)
+        => _reader.GetFloat(i);
+    public Guid GetGuid(int i)
+        => _reader.GetGuid(i);
+    public short GetInt16(int i)
+        => _reader.GetInt16(i);
+    public int GetInt32(int i)
+        => _reader.GetInt32(i);
+    public long GetInt64(int i)
+        => _reader.GetInt64(i);
+    public string GetName(int i)
+        => _fieldNames[i];
+    public int GetOrdinal(string name)
+    {
+        var index = Array.IndexOf(_fieldNames, name);
+        if (index < 0)
+            throw new IndexOutOfRangeException($"Field '{name}' not found.");
+        return index;
+    }
+
+    public string GetString(int i)
+        => _reader.GetString(i);
+    public object GetValue(int i)
+        => _reader.GetValue(i);
+    public int GetValues(object[] values)
+        => _reader.GetValues(values);
+    public bool IsDBNull(int i)
+        => _reader.IsDBNull(i);
+}
+
