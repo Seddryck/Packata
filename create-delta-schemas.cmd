d132892b09a42d4bf2f81ba7c321e58e9ce9c5e7
Cédric L. Charlier <seddryck@gmail.com>
2025-05-08 19:29:10 +0200

    fix: path for binaries to extract class definition

--- create-delta-schemas.cmd
+++ create-delta-schemas.cmd
@@ -1,4 +1,5 @@
-schemathief delta -a ./src/Packata.Core/bin/debug/net8.0/Packata.Core.dll -c Packata.Core.TableDialect -b https://datapackage.org/profiles/2.0/tabledialect.json -x "paths|profile" > ./.schemas/tabledialect.json
-schemathief delta -a ./src/Packata.Core/bin/debug/net8.0/Packata.Core.dll -c Packata.Core.Schema -b https://datapackage.org/profiles/2.0/tableschema.json -x "paths|profile" > ./.schemas/tableschema.json
-schemathief delta -a ./src/Packata.Core/bin/debug/net8.0/Packata.Core.dll -c Packata.Core.Resource -b https://datapackage.org/profiles/2.0/dataresource.json -x "paths|profile" > ./.schemas/dataresource.json
-schemathief delta -a ./src/Packata.Core/bin/debug/net8.0/Packata.Core.dll -c Packata.Core.DataPackage -b https://datapackage.org/profiles/2.0/datapackage.json -x "paths|profile" > ./.schemas/datapackage.json
+
+schemathief delta -a .\src\Packata.Core\bin\Release\net8.0\Packata.Core.dll -c Packata.Core.TableDialect -b https://datapackage.org/profiles/2.0/tabledialect.json -x "paths|profile" > .\.schemas\tabledialect.json
+schemathief delta -a .\src\Packata.Core\bin\Release\net8.0\Packata.Core.dll -c Packata.Core.Schema -b https://datapackage.org/profiles/2.0/tableschema.json -x "paths|profile" > .\.schemas\tableschema.json
+schemathief delta -a .\src\Packata.Core\bin\Release\net8.0\Packata.Core.dll -c Packata.Core.Resource -b https://datapackage.org/profiles/2.0/dataresource.json -x "paths|profile" > .\.schemas\dataresource.json
+schemathief delta -a .\src\Packata.Core\bin\Release\net8.0\Packata.Core.dll -c Packata.Core.DataPackage -b https://datapackage.org/profiles/2.0/datapackage.json -x "paths|profile" > .\.schemas\datapackage.json
