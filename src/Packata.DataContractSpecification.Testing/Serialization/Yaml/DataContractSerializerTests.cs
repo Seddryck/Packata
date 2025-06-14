a920f2aba2aa9f7b4f1291b8ade6028a63746edb
codefactor-io <support@codefactor.io>
2025-06-14 18:07:31 +0000

    [CodeFactor] Apply fixes

--- src/Packata.DataContractSpecification.Testing/Serialization/Yaml/DataContractSerializerTests.cs
+++ src/Packata.DataContractSpecification.Testing/Serialization/Yaml/DataContractSerializerTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -258,6 +258,5 @@ public class DataContractSerializerTests : BaseDataContractSerializerTests
             Assert.That(pgCustomers.Database, Is.EqualTo("crm"));
             Assert.That(pgCustomers.Schema, Is.EqualTo("main"));
         });
-
     }
 }
